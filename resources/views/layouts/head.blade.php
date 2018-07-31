3a5f00ed3935f36cb4a10e87f0747d0b6e7cd233
Ez_lan <eizs798@gmail.com>
2018-08-01 01:28:46 +0800

    Feat | online css

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.2/litera/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/agency.css') }}">
+    <link rel="stylesheet" href="https://blackrockdigital.github.io/startbootstrap-agency/css/agency.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
