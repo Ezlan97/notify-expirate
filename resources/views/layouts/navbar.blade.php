56687603ddce190296d83d5d8622b01d090d5d23
Ez_lan <eizs798@gmail.com>
2018-08-08 08:56:27 +0800

    Feat | update image bg

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,12 +7,6 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav text-uppercase ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#about">About</a>
-                </li>
                 @if(Auth::guest())
                 <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a>
