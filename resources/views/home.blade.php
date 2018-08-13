23d1326465f7b27f4bf760a82c94c5e0e1c9adb1
Ez_lan <eizs798@gmail.com>
2018-08-13 08:59:17 +0800

    Feat | update code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -287,9 +287,9 @@
                             @endif
                         </div>
                     </div>
-
+                    
                     <div class="form-group row">
-                        <label for="secondEmail" class="col-md-4 col-form-label text-md-right">Second Email</label>
+                        <label for="secondEmail" class="col-md-4 col-form-label text-md-right">Second E-mail</label>
                         
                         <div class="col-md-6">
                             <input id="secondEmail" type="email" class="form-control{{ $errors->has('secondEmail') ? ' is-invalid' : '' }}" name="secondEmail" value="{{ Auth::user()->secondEmail }}">
